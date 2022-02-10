76535c699af3069fb624245286ba95aa497b7472
Parthita <90266945+Parthita@users.noreply.github.com>
2022-02-10 14:12:45 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,95 @@
+import mysql.connector as c
+myc=c.connect(host="localhost",user="root",passwd="",database="user_database")
+cursor=myc.cursor()
+print("Welcome to XYZ")
+def entry():
+    print("If you are a new user enter 0")
+    print("If you are a existing user enter 1")
+    new_old_user = int(input("Enter the number:"))
+    if new_old_user == 0:
+        new_entry()
+    elif new_old_user == 1:
+        old_entry_name()
+    else:
+        print("Enter a valid number")
+        entry()
+def old_entry_name():
+    old_entry_name.old_name= input("Please enter your name:")
+    cursor.execute("select name from user_data")
+    name = cursor.fetchall()
+    on=1
+    for check_name in name:
+        if old_entry_name.old_name== check_name[0]:
+            print("User Found")
+            old_password_entry()
+            on=0
+            break
+    if on==1:
+        print("User not found")
+        old_entry_name()
+def old_password_entry():
+    old_password = input("Please enter your password:")
+    cp="select password from user_data where name=(%s)"
+    cn=old_entry_name.old_name
+    cursor.execute(cp,(cn,))
+    password = cursor.fetchall()
+    for check_password in password:
+        old_password_entry.op=1
+
+        for j in range(len(password)):
+            if old_password==check_password[0]:
+                print("Correct Password")
+                old_password_entry.op=0
+                break
+            elif old_password_entry.op==1:
+                print("Wrong Password")
+                old_password_entry()
+        break
+def new_entry():
+    ep_1 = "INSERT INTO user_data (Name,Password,study_time) VALUES (%s,%s,%s)"
+    new_name = input("Please enter your name")
+    new_password = input("Please enter your password")
+    new_time=int(input("How much time you want to study"))
+    ep_2 = (new_name, new_password,new_time)
+    cursor.execute(ep_1, ep_2)
+    myc.commit()
+def time_calc():
+    a=int(input("Hello User how much time u studied today?:"))
+    ct=("Select study_time from user_data where name=(%s)")
+    cursor.execute(ct,(old_entry_name.old_name,))
+    time=cursor.fetchall()
+    for abc in time:
+        timec=abc[0]
+
+
+    if a < timec:
+        print("You studied ",timec-a,"hours less today")
+        print("Would you like to add todays waste to tommorows schedule to makeup")
+        #bakitatorakoris
+    elif a > timec:
+        print("You studied ",a-timec,"hours less today")
+        print("Studying more is not bad")
+        print("Would you like to cut from tommorows schedule")
+        #baki ta tora koris
+    elif a==timec:
+        print("Congratssss!!!!! ")
+        print("You satisfied your schedule today")
+
+
+entry()
+if old_password_entry.op==0:
+    time_calc()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
